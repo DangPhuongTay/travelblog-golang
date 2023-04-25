87442f619d10aa5aa530467af9c31afcf1e1f37c
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2023-04-26 02:16:49 +0700

    fix sever

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
-	"log"
-	"os"
+	"net/http"
 
 	"github.com/DangPhuongTay/travelblog-golang/database"
-	"github.com/DangPhuongTay/travelblog-golang/routes"
-	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
 	database.Connect()
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	port := os.Getenv("PORT")
-	app := fiber.New()
-	routes.Setup(app)
-	app.Listen(":" + port)
+
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// port := os.Getenv("PORT")
+	// app := fiber.New()
+	// routes.Setup(app)
+	// app.Listen(":" + port)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
 }
